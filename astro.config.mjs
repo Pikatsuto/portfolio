bbb48b0c944ca61dc2944f391084d1d14c285af6
Gabriel Guillou <pikatsuto@gmail.com>
2026-02-26 23:45:11 +0100

    Add SEO meta tags and integrate search into BaseLayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import node from "@astrojs/node";
 
 export default defineConfig({
+  site: "https://gabriel-guillou.fr",
   output: "server",
   adapter: node({ mode: "standalone" }),
   integrations: [react()],
